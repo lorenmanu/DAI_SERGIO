b7910eb5a8949d3a175eb4d788df5c6c0c66f38b
loremanu <lorenrr1@gmail.com>
2016-02-10 16:26:43 +0100

    probando heroku

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
